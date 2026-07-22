1e4deccdc6d04a2438cb79c1ffd5441e6d64d4b2
Obianuju Florence <obianujuflorence01@gmail.com>
2026-07-22 09:43:41 -0400

    feat(persistence): implement DynamoDB learning repository

--- backend/repositories/dynamodb/index.ts
+++ backend/repositories/dynamodb/index.ts
@@ -1,3 +1,4 @@
 export * from './base-dynamodb-repository';
 export * from './dynamodb-workflow-repository';
 export * from './dynamodb-report-repository';
+export * from './dynamodb-learning-repository';
