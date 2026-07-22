2e90de301b563e9f1d84ce5e9d36dae4bf3dd31a
Obianuju Florence <obianujuflorence01@gmail.com>
2026-07-22 09:54:14 -0400

    feat(persistence): implement DynamoDB verification repository

--- backend/repositories/dynamodb/index.ts
+++ backend/repositories/dynamodb/index.ts
@@ -2,3 +2,4 @@ export * from './base-dynamodb-repository';
 export * from './dynamodb-workflow-repository';
 export * from './dynamodb-report-repository';
 export * from './dynamodb-learning-repository';
+export * from './dynamodb-verification-repository';
